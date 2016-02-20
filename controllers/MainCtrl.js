a07861c2bb7bf56bb6879f79886bde350e0cd067
Dustin Myers <dustint314@gmail.com>
2016-02-20 08:06:15 -0700

    finished for review

---
+++ controllers/MainCtrl.js
@@ -0,0 +1,9 @@
+app.controller('MainCtrl', ['$scope', 'dataService', function($scope, dataService) {
+
+    $scope.letsGetSomeData = function() {
+        $scope.superHeroes = dataService.getMeDatData();
+    };
+
+    $scope.letsGetSomeData();
+
+}]); //end MainCtrl
