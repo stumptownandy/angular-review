4872eab1a040cbcee9cdbe87693d5e9ea073c5f3
Andrew Jenkins <stumptownandy@gmail.com>
2016-02-23 19:19:22 -0700

    superheros

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-var app = angular.module('starwars', []);
+var app = angular.module('avengers', ['ui.router']);
+
+app.config([
+ '$stateProvider',
+   '$urlRouterProvider', 
+
+    function($stateProvider,$urlRouterProvider){
+    $stateProvider
+        
+        .state('cards',{
+        url: '/cards',
+        templateUrl: '/views/cards.html',
+        controller:''
+        })
+        .state('hero',{
+        url: '',
+        templateUrl: '/views/hero.html',
+        controller:''
+        })
+}]);
